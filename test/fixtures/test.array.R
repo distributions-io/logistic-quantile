0f9001ec097751ddcadd7eee9645c09b58114cb1
Planeshifter <pburckhardt@outlook.com>
2015-07-21 08:54:05 +0200

    [INIT]

---
+++ test/fixtures/test.array.R
@@ -0,0 +1,19 @@
+options( digits = 16 );
+library( jsonlite );
+
+
+mu = 0
+s = 1
+probs = seq( 0, 1, 0.1 )
+y = qlogis( probs, mu, s )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	mu = mu,
+	s = s,
+	data = probs,
+	expected = y
+)
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/array.json" )
---
+++ test/fixtures/test.matrix.R
@@ -0,0 +1,19 @@
+options( digits = 16 );
+library( jsonlite );
+
+mu = 0
+s = 1
+probs = 0:24 / 25
+y = qlogis( probs, mu, s )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	mu = mu,
+	s = s,
+	data = probs,
+	expected = y
+)
+
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/matrix.json" )
